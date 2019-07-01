b66cec47f4bf5dfab00d932916767e0e85cfda0a
Carl Colglazier <carl@ibm.com>
2019-07-01 11:20:16 -0400

    Update build script

--- build.el
+++ build.el
@@ -1,4 +1,10 @@
 ;; TODO: Probably better to install the package.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(setq package-selected-packages '(ox-hugo))
+(package-refresh-contents)
+(package-install-selected-packages)
 (require 'ox-hugo)
 (switch-to-buffer (find-file-noselect "content-org/writing.org"))
 (org-hugo-export-wim-to-md :all-subtrees)
